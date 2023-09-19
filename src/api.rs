37f3401c6426e74b533f52664b40b66b7ab37bde
Xavier Lau <xavier@inv.cafe>
2023-09-19 17:19:30 +0800

    Release `v0.1.0`

---
+++ src/api.rs
@@ -0,0 +1,149 @@
+// crates.io
+use parity_scale_codec::Decode;
+use reqwest::{
+	header::{HeaderMap, CONTENT_TYPE},
+	Client, ClientBuilder,
+};
+use serde::Deserialize;
+use subrpcer::state;
+// commission-board
+use crate::*;
+
+pub struct Api {
+	client: Client,
+}
+impl Api {
+	pub fn new() -> Self {
+		Self {
+			client: ClientBuilder::new()
+				.default_headers(HeaderMap::from_iter([(
+					CONTENT_TYPE,
+					"application/json".parse().unwrap(),
+				)]))
+				.build()
+				.unwrap(),
+		}
+	}
+
+	pub async fn validators(&self) -> Result<Vec<String>> {
+		let json = state::get_storage(
+			0,
+			"0xcec5070d609dd3497f72bde07fc96ba088dcde934c658227ee1dfafcd6e16903",
+			None::<()>,
+		);
+		let result = self
+			.client
+			.post("https://darwinia-rpc.darwiniacommunitydao.xyz")
+			.json(&json)
+			.send()
+			.await?
+			.json::<JsonrpcResult>()
+			.await?;
+		let validators =
+			<Vec<[u8; 20]>>::decode(&mut &*array_bytes::hex2bytes_unchecked(result.result))
+				.unwrap()
+				.into_iter()
+				.map(|v| array_bytes::bytes2hex("0x", v))
+				.collect();
+
+		Ok(validators)
+	}
+
+	pub async fn commission_history_of(&self, who: &str) -> Result<CommissionHistory> {
+		let body = format!(
+			"{{\
+				\"query\":\"{{\
+						events(\
+						where:{{\
+							name_eq:\\\"DarwiniaStaking.CommissionUpdated\\\",\
+							args_jsonContains:\\\"{{\\\\\\\"who\\\\\\\":\\\\\\\"{}\\\\\\\"}}\\\"\
+						}},\
+						limit:5,\
+						orderBy:block_height_DESC\
+					){{\
+						args \
+						block{{height}}\
+					}}\
+				}}\"\
+			}}",
+			who
+		);
+		let response = self
+			.client
+			.post("https://darwinia.explorer.subsquid.io/graphql")
+			.body(body)
+			.send()
+			.await?
+			.json::<Data>()
+			.await?;
+
+		Ok(CommissionHistory {
+			who: who.into(),
+			commissions: response
+				.data
+				.events
+				.into_iter()
+				.map(|d| (d.block.height, d.args.commission))
+				.collect(),
+		})
+	}
+}
+
+#[derive(Debug, Deserialize)]
+struct JsonrpcResult {
+	result: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Data {
+	data: DataInner,
+}
+#[derive(Debug, Deserialize)]
+struct DataInner {
+	events: Vec<Event>,
+}
+#[derive(Debug, Deserialize)]
+struct Event {
+	args: Commission,
+	block: Block,
+}
+#[derive(Debug, Deserialize)]
+struct Commission {
+	commission: u32,
+}
+#[derive(Debug, Deserialize)]
+struct Block {
+	height: u32,
+}
+
+#[derive(Debug)]
+pub struct CommissionHistory {
+	pub who: String,
+	pub commissions: Vec<(u32, u32)>,
+}
+impl CommissionHistory {
+	pub fn commissions(&self) -> String {
+		if self.commissions.is_empty() {
+			return "Never changed, amazing!".into();
+		}
+
+		self.commissions
+			.iter()
+			.map(|(h, c)| format!("({h},{}%)", *c as f64 / 10000000.))
+			.collect::<Vec<_>>()
+			.join(", ")
+	}
+
+	pub fn reputation(self) -> &'static str {
+		for w in self.commissions.windows(3) {
+			let (h_0, _) = &w[0];
+			let Some((h_2, _)) = w.get(2) else { return "Good" };
+
+			if h_0 - h_2 <= 7200 {
+				return "Bad";
+			}
+		}
+
+		"Good"
+	}
+}
---
+++ src/main.rs
@@ -0,0 +1,105 @@
+mod api;
+use api::Api;
+
+pub use anyhow::Result;
+
+// std
+use std::env;
+// crates.io
+use actix_web::{App, HttpResponse, HttpServer, Responder};
+use futures::future;
+use once_cell::sync::Lazy;
+
+static API: Lazy<Api> = Lazy::new(Api::new);
+
+#[actix_web::get("/")]
+async fn query() -> impl Responder {
+	let mut data = String::new();
+
+	if let Ok(vs) = API.validators().await {
+		future::join_all(vs.into_iter().map(|v| async move {
+			let commission_history = loop {
+				if let Ok(c) = API.commission_history_of(&v).await {
+					break c;
+				} else {
+					continue;
+				}
+			};
+
+			format!(
+				r#"<tr><td><a href="https://darwinia.subscan.io/account/{v}">{v}</td><td>{}</td><td>{}</td></tr>"#,
+				commission_history.commissions(),
+				commission_history.reputation()
+			)
+		}))
+		.await
+		.into_iter()
+		.for_each(|d| data.push_str(&d));
+	}
+
+	let body = format!(
+		r#"<!DOCTYPE html>
+        <html>
+          <head>
+            <title>Commission Board</title>
+            <style>
+              html, body {{
+                height: 100%;
+              }}
+
+              body {{
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background-color: #1c2331;
+                color: #fff;
+                font-family: monospace;
+				font-size: 18px;
+              }}
+
+              th:nth-child(1), td:nth-child(1) {{
+                background-color: #2c3e50;
+              }}
+
+              th:nth-child(2), td:nth-child(2) {{
+                background-color: #2980b9;
+              }}
+
+              th:nth-child(3), td:nth-child(3) {{
+                background-color: #27ae60;
+              }}
+
+              th, td {{
+                text-align: center;
+              }}
+
+              a {{
+                color: white;
+              }}
+            </style>
+          </head>
+          <body>
+            <table style="margin: 0 auto;">
+              <tr>
+                <th>Validator</th>
+                <th>Commission History (Block,Value)</th>
+                <th>Reputation Base on Recent 5 Changes</th>
+              </tr>
+              {data}
+            </table>
+          </body>
+        </html>"#,
+	);
+
+	HttpResponse::Ok().body(body)
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+	HttpServer::new(|| App::new().service(query))
+		.bind(("0.0.0.0", env::var("COMMISSION_BOARD_PORT")?.parse()?))?
+		.run()
+		.await?;
+
+	Ok(())
+}
